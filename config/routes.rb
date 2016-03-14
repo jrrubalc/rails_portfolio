c325a3a1a1d57344fa66a066ec8e0036e1fe1735
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-03-14 02:00:56 +0000

    Adds project pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'projects/summary'
+
+  get 'pages/projects'
+
   get 'welcome/homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
